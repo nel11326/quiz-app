401e68e808d4176987e4cb452346c02f2d5d065b
nel11326 <lenor11326@hotmail.com>
2024-09-16 15:32:06 +0800

    summary(create): outputting quiz results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,10 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
+
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
@@ -25,12 +27,7 @@ export default function Quiz() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Quiz complete" />
-        <h2>Quiz Complete</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
